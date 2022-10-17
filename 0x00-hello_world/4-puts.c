663159d2b7ff313bb391bd7c1228280a4c3c8dbd
chidimma042 <chidimmaominyi042@gmail.com>
2022-10-16 20:10:59 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 /*
  * main - print a line of code using puts
  * Return: 0
-/*
- * int main(void)
+*/
+int main(void)
 {
  	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
